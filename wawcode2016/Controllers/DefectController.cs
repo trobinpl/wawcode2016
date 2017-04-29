c74af25826d24129e1b2fa15568751e8e3a1379e
Paweł Hemperek <pawel+github@hemperek.pl>
2017-04-29 14:41:59 +0200

    Dodanie wysyłki maili po dodaniu usterki.

--- wawcode2016/Controllers/DefectController.cs
+++ wawcode2016/Controllers/DefectController.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using RestSharp;
+using RestSharp.Authenticators;
+using SMSApi.Api;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -32,7 +36,30 @@ namespace wawcode2016.Controllers
                 ImageUrl = defectViewModel.ImageUrl
             };
 
+            //Client smsClient = new Client("pawel+smsapi@hemperek.pl");
+            //smsClient.SetPasswordHash("909c655e3677bbba306c19edbdfd80a1");
 
+            //var smsApi = new SMSFactory(smsClient);
+
+            //var result = smsApi.ActionSend()
+            //    .SetText($"Mieszkanie: Ząbkowicka 33C. Zgłoszono nową usterkę. Opis: { defect.Description }")
+            //    .SetTo("664660800")
+            //    .SetSender("Alert")
+            //    .Execute();
+
+            var restClient = new RestClient("https://api.mailgun.net/v3");
+            restClient.Authenticator = new HttpBasicAuthenticator("api", "key-ad33d50d4b3f693639b663f64c81df4d");
+            var request = new RestRequest(Method.POST);
+            request.AddParameter("domain", "sandbox8eb74b7940404e57b90996353993faca.mailgun.org", ParameterType.UrlSegment);
+            request.Resource = "{domain}/messages";
+            request.AddParameter("from", "Flateo <alert@flat.eo>");
+            request.AddParameter("to", "pawel+flateo@hemperek.pl");
+            request.AddParameter("subject", "Nowa usterka - Ząbkowicka 33C");
+            request.AddParameter("text", $"Zgłoszono nową usterkę. Opis: {defect.Description}");
+
+            var emailResult = restClient.Execute(request);
+
+            //Debug.WriteLine(result);
         }
 
         // PUT api/<controller>/5
